7e462967c1e35bd3b6034f5b6fef1bfe9db7ca85
Tom <tom@cnvr.io>
2016-02-08 14:53:55 +0000

    eFFT cpp class
    Version 1.  Tested and works fine using g++ 11

--- cpp/eFFT.h
+++ cpp/eFFT.h
@@ -2,17 +2,19 @@
 //cpp header
 //T.Lloyd & S.Ismail
 //2015-2016
-//test comment
+
 
 #include <cmath>
 #include <complex>
 class eFFT
 {
 private:
-		char * binaryadd(char *bits, unsigned int position, unsigned int totalbits);
-		int * bitrev(int * order, unsigned int bits, int N, int div);
-		std::complex<double> * butterfly(std::complex<double> * input, int siglen, int norm, int sign);
+	char * binaryadd(char *bits, unsigned int position, unsigned int totalbits);
+	int * bitrev(int * order, unsigned int bits, int N, int div);
+	std::complex<double> * butterfly(std::complex<double> * input, int siglen, int norm, int sign);
 public:
 	std::complex<double> * fft(std::complex<double> * input,int length);
 	std::complex<double> * ifft(std::complex<double> * input,int length);
-}
+	eFFT();
+	~eFFT();
+};
