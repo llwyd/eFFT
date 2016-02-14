1ae7609dfff66136644be604f045b79efc08499d
llwyd <tom@llwyd.io>
2016-02-14 00:35:28 +0000

    Minor formatting changes

--- c/eFFT.h
+++ c/eFFT.h
@@ -1,5 +1,8 @@
 //eFFT.h
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <complex.h>
+#include <math.h>
 
 double complex * fft(double complex * input,int length);
 double complex * ifft(double complex * input,int length);
