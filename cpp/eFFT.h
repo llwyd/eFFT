f54a2947f784d751899ff80734ae6847e517da56
Tom <tom@llwyd.io>
2016-03-01 18:35:59 +0000

    Further Improvements
    Removed binaries and made changes so that the c++ files acted more like
    a library and less as an object.

--- cpp/eFFT.h
+++ cpp/eFFT.h
@@ -1,20 +1,26 @@
+#ifndef EFFT_H
+#define EFFT_H
+
 //eFFT.h
 //cpp header
 //T.Lloyd & S.Ismail
 //2015-2016
 
-
 #include <cmath>
 #include <complex>
+
+namespace eFFT
+{
 class eFFT
 {
 private:
-	int * rrotate(int * input,unsigned int bits, int n);
-	int * bitrev(int * order, unsigned int bits, int n);
-	std::complex<double> * butterfly(std::complex<double> * input, int siglen, int norm, int sign);
+	static int * rrotate(int * input,unsigned int bits, int n);
+	static int * bitrev(int * order, unsigned int bits, int n);
+	static std::complex<double> * butterfly(std::complex<double> * input, int siglen, int norm, int sign);
 public:
-	std::complex<double> * fft(std::complex<double> * input,int length);
-	std::complex<double> * ifft(std::complex<double> * input,int length);
-	eFFT();
-	~eFFT();
+	static std::complex<double> * fft(std::complex<double> * input,int length);
+	static std::complex<double> * ifft(std::complex<double> * input,int length);
 };
+}
+
+#endif // EFFT_H
